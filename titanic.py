6eb3c68bed148984894157f34ca734b906008acf
Alexandru Dima <dima.alexandru14@gmail.com>
2024-05-13 13:00:12 +0300

    Implement task 3 and 4

--- titanic.py
+++ titanic.py
@@ -15,10 +15,11 @@ titanic = pd.read_csv('data/train.csv')
 print(f'Number of lines and columns: {nr_rows}, {nr_cols}')
 print()
 # Show data types for each column
-print(titanic.dtypes)
+columns = titanic.columns.tolist()
+for col in columns:
+    print(f"{col} type -> {titanic[col].dtype}")
 print()
 # Count missing values in each column
-columns = titanic.columns.tolist()
 for col in columns:
     full_rows = titanic[col].dropna().count()
     print(f"Column '{col}' missing {nr_rows - full_rows} values")
@@ -30,8 +31,8 @@ print()
 
 # TASK 2
 print('-----TASK 2-----')
-print()
 print('Results in graphs/')
+print()
 survivors = titanic[titanic['Survived'] == 1].shape[0]
 survival_rate = survivors * 100 / nr_rows
 death_rate = 100 - survival_rate
@@ -44,13 +45,54 @@ third_class_percentage = third_class * 100 / nr_rows
 males = titanic[titanic['Sex'] == 'male'].shape[0]
 male_percentage = males * 100 / nr_rows
 female_percentage = 100 - male_percentage
-plt.pie([survival_rate, death_rate], labels=['Survivors', 'Dead'], autopct='%.2f%%')
-plt.savefig('graphs/survival_rate.png')
-plt.close()
-class_list = [first_class_percentage, second_class_percentage, third_class_percentage]
-plt.pie(class_list, labels=['First class', 'Second class', 'Third class'], autopct='%.2f%%')
-plt.savefig('graphs/class_distribution.png')
-plt.close()
-plt.pie([male_percentage, female_percentage], labels=['Males', 'Females'], autopct='%.2f%%')
-plt.savefig('graphs/gender_distribution.png')
-plt.close()
+fig, axes = plt.subplots(nrows = 1, ncols = 3)
+values_list = [survival_rate, death_rate]
+plt.rcParams.update({'font.size': 8})
+axes[0].pie(values_list, labels=['Survived', 'Dead'], autopct='%.2f%%')
+axes[0].set_title('Survival Rate', fontsize = 12)
+values_list = [first_class_percentage, second_class_percentage, third_class_percentage]
+axes[1].pie(values_list, labels=['First class', 'Second class', 'Third class'], autopct='%.2f%%')
+axes[1].set_title('Class Distribution', fontsize = 12)
+values_list = [male_percentage, female_percentage]
+axes[2].pie(values_list, labels=['Males', 'Females'], autopct='%.2f%%')
+axes[2].set_title('Gender Distribution', fontsize = 12)
+plt.tight_layout()
+plt.savefig('graphs/task-2-stats.png')
+
+# TASK 3
+print('-----TASK 3-----')
+print('Results in graphs/')
+print()
+for col in columns:
+    if (titanic[col].dtype in ('int64', 'float64') and col != 'PassengerId'):
+        plt.figure()
+        titanic[col].hist()
+        plt.title(col, fontsize = 12)
+        plt.xlabel(col, fontsize = 10)
+        plt.ylabel('Number of people', fontsize = 10)
+        plt.savefig(f"graphs/{col}.png")
+
+# TASK 4
+print('-----TASK 4-----')
+print()
+for col in columns:
+    if titanic[col].dropna().count() != nr_rows:
+        missing = nr_rows - titanic[col].dropna().count()
+        proportion = missing * 100 / nr_rows
+        print(f"{col} missing {proportion:.2f}% values ({missing} out of {nr_rows})")
+        temp = titanic[['Survived', col]]
+        temp = temp[temp.isnull().any(axis = 1)]
+        missing_survived = temp[temp['Survived'] == 1].shape[0]
+        proportion = missing_survived * 100 / nr_rows
+        print(f"Survived: {proportion:.2f}%")
+        missing_dead = temp[temp['Survived'] == 0].shape[0]
+        proportion = missing_dead * 100 / nr_rows
+        print(f"Died: {proportion:.2f}%")
+print()
+
+# TASK 5
+print('-----TASK 5-----')
+print()
+plt.figure()
+titanic['Age'].hist(bins = [0, 20, 40, 60, titanic['Age'].max()])
+plt.savefig('graphs/task-5-ages.png')

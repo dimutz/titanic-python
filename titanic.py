ab11bfc18c80fedba2d86eb83869a3db761cbbad
Alexandru Dima <dima.alexandru14@gmail.com>
2024-05-30 18:31:58 +0300

    Implemented task 8 and 9

--- titanic.py
+++ titanic.py
@@ -2,6 +2,7 @@
 Train Titanic Dataset
 """
 
+import re
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -51,7 +52,7 @@ female_percentage = 100 - male_percentage
 fig, axes = plt.subplots(nrows = 1, ncols = 3)
 values_list = [survival_rate, death_rate]
 plt.rcParams.update({'font.size': 8})
-axes[0].pie(values_list, labels=['Survived', 'Dead'], autopct='%.2f%%')
+axes[0].pie(values_list, labels=['Survived', 'Died'], autopct='%.2f%%')
 axes[0].set_title('Survival Rate', fontsize = 12)
 values_list = [first_class_percentage, second_class_percentage, third_class_percentage]
 axes[1].pie(values_list, labels=['First class', 'Second class', 'Third class'], autopct='%.2f%%')
@@ -105,14 +106,13 @@ print('-----TASK 5-----')
 print()
 # Set bins and indexes for separating age categories
 bins = [0, 20, 40, 60, titanic['Age'].max() + 1]
-labels = [1, 2, 3, 4]
+labels = ['1', '2', '3', '4']
+# Add new column
 titanic['AgeCat'] = pd.cut(titanic['Age'], bins = bins, labels = labels, right = False)
 # Count passagers in each age category
 age_counts = titanic['AgeCat'].value_counts().sort_index()
 print("Number of people in each age category:")
 print(age_counts)
-# Add new column
-titanic.to_csv('data/new_train.csv')
 plt.figure()
 categories = ['1', '2', '3', '4']
 bars = plt.bar(categories, age_counts.values)
@@ -164,8 +164,52 @@ bars = plt.bar(categories, values)
 for bar in bars:
     yval = bar.get_height()
     plt.text(bar.get_x() + bar.get_width()/2, yval + 0.3, f"{yval:.2f}%", ha='center', va='bottom')
-plt.title('Children vs. Adult survival rates', fontsize = 12)
+plt.title('Children vs. Adults survival rates', fontsize = 12)
 plt.ylabel('Survival rate', fontsize = 10)
 plt.savefig('graphs/task-7-stats.png')
 plt.close()
+print()
+
+# TASK 8
+print('-----TASK 8-----')
+print()
+for col in columns:
+    if nr_rows - titanic[col].dropna().count() != 0:
+        if titanic[col].dtype == 'int64':
+            titanic[col] = titanic[col].transform(lambda x: x.fillna(int(x.mean())))
+        elif titanic[col].dtype == 'float64':
+            titanic[col] = titanic[col].transform(lambda x: x.fillna(x.mean()))
+        else:
+            column_values = titanic[col].value_counts().sort_values(ascending=False)
+            predominant_value = column_values.keys()[0].split()[0]
+            titanic[col] = titanic[col].transform(lambda x: x.fillna(predominant_value))
+titanic.to_csv('data/new_train.csv')
+print("Done")
+print()
+
+# TASK 9
+print('-----TASK 9-----')
+print()
+
+def extract_title(name):
+    """
+       Function for extracting the title
+    """
+    title_search = re.search(' ([A-Za-z]+)\.', name)
+    if title_search:
+        return title_search.group(1)
+    return ""
+
+titanic['Title'] = titanic['Name'].apply(extract_title)
+sex_check = titanic.groupby(['Title', 'Sex']).size().unstack(fill_value = 0)
+print(sex_check)
+plt.figure(figsize = (12,6))
+bars = plt.bar(titanic['Title'].value_counts().index, titanic['Title'].value_counts().values)
+for bar in bars:
+    yval = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width()/2, yval + 0.3, yval, ha='center', va='bottom')
+plt.title('Title count', fontsize = 12)
+plt.ylabel('Number of people', fontsize = 10)
+plt.xticks(rotation = 45)
+plt.savefig('graphs/task-9-stats.png')
 print()
